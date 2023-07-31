170e101a1fceeee14ebd4584af11007467d6af5d
Ziaaaaaaaaaaaa <ziakhan29042003@gmail.com>
2023-07-31 15:58:55 +0200

    q2 done

---
+++ Person.js
@@ -0,0 +1,9 @@
+function Person(firstName, lastName, emailAddress) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emailAddress = emailAddress;
+  }
+  
+  module.exports = Person;
+
+
---
+++ index.js
@@ -0,0 +1,9 @@
+const Person = require('./Person');
+
+const person1 = new Person('zia', 'khan', 'zia.khan@example.com');
+const person2 = new Person('justin', 'scholtz', 'justin.scholtz@example.com');
+const person3 = new Person('jade', 'de wet', 'jade.de wet@example.com');
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
